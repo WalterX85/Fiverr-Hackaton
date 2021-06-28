a3c41a736f04de6e173ebc7cea99bb8bc4ca3b04
cyrildav <cyril.david51@hotmail.fr>
2021-06-28 17:21:03 +0200

    get on users

--- back/index.js
+++ back/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const userRoutes = require('./routes/users');
+
+app.use('/users', userRoutes);
 const port = 9000;
 
-app.listen(9000, () => {
+app.listen(port, () => {
   console.log(`server is running on ${port}`);
 });
