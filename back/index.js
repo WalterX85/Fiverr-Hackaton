7c77dd86b66a94d06bae18fcb326a94bd4747efc
cyrildav <cyril.david51@hotmail.fr>
2021-06-29 11:55:22 +0200

    add request post

--- back/index.js
+++ back/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const questionsRoutes = require('./routes/questions');
 const userRoutes = require('./routes/users');
--- back/routes/questions.js
+++ back/routes/questions.js
@@ -23,4 +23,35 @@ questions.get('/:id/answers', (req, res) => {
   });
 });
 
+questions.post('/', (req, res) => {
+  const question = {
+    question_text: req.body.question_text,
+    user_id: req.body.user_id,
+  };
+
+  db.query('INSERT INTO question (question_text, user_id) VALUES (?, ?)', [question.question_text, question.user_id], (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500);
+    } else {
+      res.status(201).json({ ...question, id: results.insertId });
+    }
+  });
+});
+
+questions.post('/:id/answers', (req, res) => {
+  const answer = {
+    user_id: req.body.user_id,
+    answer_text: req.body.answer_text,
+  };
+
+  db.query('INSERT INTO answer (question_id, user_id, answer_text) VALUES (?, ?, ?)', [req.params.id, answer.user_id, answer.answer_text], (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500);
+    } else {
+      res.status(201).json({ ...answer, id: results.insertId });
+    }
+  });
+});
 module.exports = questions;
